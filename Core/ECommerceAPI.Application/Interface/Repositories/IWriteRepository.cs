afece538ee762fd232923c57e50afa9139605a5e
omeradiyaman1169@gmail.com <omeradiyaman1169@gmail.com>
2024-05-23 17:56:54 +0300

    Read repository yapıldı

---
+++ Core/ECommerceAPI.Application/Interface/Repositories/IWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Interface.Repositories
+{
+    public interface IWriteRepository
+    {
+
+    }
+}
